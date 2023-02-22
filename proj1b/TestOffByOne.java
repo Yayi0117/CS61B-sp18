a6527dea34f29d31b7313891d4b0b37985a8adbe
Yayi0117 <joycewang1403@gmail.com>
2023-02-22 10:32:46 +0800

    finished project1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,5 +16,7 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'A'));
+        assertFalse(offByOne.equalChars('A', 'e'));
     }
 }
